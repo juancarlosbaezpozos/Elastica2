c18088a67c0bd6fbc71f1e679f1083fac81785de
juancarlosbaezpozos <jbaez@outlook.com>
2018-03-28 17:03:47 -0600

    ultimos cambios

--- src/connect.ts
+++ src/connect.ts
@@ -5,26 +5,3 @@ export default async function (): Promise<Db> {
     const uri = "mongodb://poco:" + contrasenia + "@poco.documents.azure.com:10255/?ssl=true";
     return await MongoClient.connect(uri);
 };
-
-/*import { Db } from "mongodb";
-const mongoClient = require("mongodb").MongoClient;
-let database = null;
-
-export default async function (): Promise<Db> {
-    return await new mongoClient('mongodb://poco.documents.azure.com:10255/?ssl=true', {
-        auth: {
-            user: 'poco',
-            password: 'LNE6iwRk3mb66M8O8bvIpD9nzTx9D5c0fDRGsFw91F8n3L3tfZ7gCmIfvfP5XnoLrLxFrzuFVVXqhbPvF38PgQ==',
-        }
-    }).connect(
-        (err, db) => {
-            if (err) {
-                db.close();
-                return console.error(err);
-            } else {
-                console.log('conectado');
-                database = db.db('test');
-            }
-        });
-};*/
-
